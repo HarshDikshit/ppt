d980cd9e36e6b642ea0a1bc7e10de7d0fb1f25ff
HarshDikshit <93562022+HarshDikshit@users.noreply.github.com>
2024-06-20 11:47:47 +0530

    sdjdvbjvb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html","*.html"],
+  content: ["./src/**/*.{html,js,css}","*.html"],
   theme: {
     extend: {},
   },
